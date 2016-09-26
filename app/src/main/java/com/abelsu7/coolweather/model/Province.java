5a967c1a640b346d7556c955c27e45af926f1404
Abel Su <abel1995@163.com>
2016-09-26 21:43:46 +0800

    Add model defination

---
+++ app/src/main/java/com/abelsu7/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.abelsu7.coolweather.model;
+
+/**
+ * Created by AbelSu on 2016/9/26.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
